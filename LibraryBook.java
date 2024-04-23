da0a8dff8c3cb8ec09444e42eb535511e0dff51d
20211814김동연 <161808977+dogang2@users.noreply.github.com>
2024-04-23 16:43:09 +0900

    시험 끝!

--- LibraryBook.java
+++ LibraryBook.java
@@ -1,31 +1,3 @@
-/*public interface LibraryBook implements Book { //
-	private String isbn;
-	private String title;
-	private String author;
-	private int year;
-	private boolean available;
-
-	public LibraryBook(Book book) {
-	    // 생성자
-		this.isbn = book.getIsbn(); ///
-		this.available = true;
-	}
-	
-	public LibraryBook(String isbn, String title, String author, int year) {
-	    // 생성자
-		this.isbn = book.getIsbn();
-		this.available = true;
-	}
-
-	@Override
-	public String getIsbn(); // return isbn
-	public String getTitle(); // return title
-	public String getAuthor(); // return author
-	public int getYear(); // return year
-	public boolean isAvailable(); // return available
-	public void setAvailable(boolean available);
-}
-*/
 public class LibraryBook implements Book {
     private String isbn;
     private String title;
